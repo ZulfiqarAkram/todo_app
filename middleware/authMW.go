789d1881131be8ee59b83e2155102ead18e02dd2
zulfiqar10p <muhammad.zulfiqar@tenpearls.com>
2019-12-31 15:58:35 +0500

    allowed to access some path without token.

--- middleware/authMW.go
+++ middleware/authMW.go
@@ -12,27 +12,33 @@ type contextKey int
 
 const AuthenticatedUserKey contextKey = 0
 
+var AllowedPathWithToken = []string{
+	"/api/login",
+	"/api/register",
+}
+
 type Authorization struct {
 	handler http.Handler
 }
 
 func (l *Authorization) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	token := r.Header.Get("token")
-	// Pass Authorization for Login and Registration
-	if token == "" {
-		l.handler.ServeHTTP(w, r)
-	} else {
-		payLoad, err, isValid := IsValidToken(token)
-		if isValid {
-			contextWithUser := context.WithValue(r.Context(), AuthenticatedUserKey, payLoad)
-			rWithUser := r.WithContext(contextWithUser)
-			l.handler.ServeHTTP(w, rWithUser)
-		} else {
-			http.Error(w, err.Error(), 401)
+	for _, path := range AllowedPathWithToken {
+		if path == r.URL.Path {
+			l.handler.ServeHTTP(w, r)
 			return
 		}
-		log.Printf("=> %s %s", r.Method, r.URL.Path)
 	}
+	payLoad, err, isValid := IsValidToken(token)
+	if isValid {
+		contextWithUser := context.WithValue(r.Context(), AuthenticatedUserKey, payLoad)
+		rWithUser := r.WithContext(contextWithUser)
+		l.handler.ServeHTTP(w, rWithUser)
+		return
+	}
+	http.Error(w, err.Error(), 401)
+	log.Printf("=> %s %s", r.Method, r.URL.Path)
+	return
 }
 
 //Constructor
